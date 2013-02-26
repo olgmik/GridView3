ebd38a9881b36dbc42bf1475658f9138071ce037
Olya <olgmik@gmail.com>
2013-02-26 01:15:43 -0500

    not working witching views on click

--- src/com/example/gridview3/MainActivityGrid3.java
+++ src/com/example/gridview3/MainActivityGrid3.java
@@ -1,16 +1,34 @@
 package com.example.gridview3;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.example.switchingviews.R;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.ScaleAnimation;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivityGrid3 extends Activity {
+	
+	ImageView imageView;
+	
+	public Integer [] pictures = {
+			R.drawable.olya_200, R.drawable.olya2_200,
+			R.drawable.olya3_200, R.drawable.olya4_200
+	}; 
+	
+	imageView = (ImageView) this.findViewById(R.id.imageView2);
+	imageView.setImageResource(pictures[position]);
 	
 	private String[] names={
 			"olya_150", "olya2_150",
@@ -25,13 +43,15 @@ public class MainActivityGrid3 extends Activity {
 		GridView gridview = (GridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new ImageAdapter(this)); 
 		
-		final ScaleAnimation scaleAnimation = new ScaleAnimation(1,2,1,2);
-		scaleAnimation.setDuration(3000);
+		//final ScaleAnimation scaleAnimation = new ScaleAnimation(1,2,1,2);
+		//scaleAnimation.setDuration(3000);
 		
 		gridview.setOnItemClickListener(new OnItemClickListener() {
 	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	        	v.startAnimation(scaleAnimation);
-	        	Toast.makeText(MainActivityGrid3.this, "" + names[position], Toast.LENGTH_SHORT).show();
+	        	setContentView(R.layout.activity_second);
+
+	        	//v.startAnimation(scaleAnimation);
+	        	//Toast.makeText(MainActivityGrid3.this, "" + names[position], Toast.LENGTH_SHORT).show();
 	        }
 	    });
 	}
