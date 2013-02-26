ce1c98c8db79eefaa5855d2bc1132a65bd82b880
Olya <olgmik@gmail.com>
2013-02-26 00:40:07 -0500

    animation-bad choice

--- src/com/example/gridview3/MainActivityGrid3.java
+++ src/com/example/gridview3/MainActivityGrid3.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.ScaleAnimation;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
@@ -24,8 +25,12 @@ public class MainActivityGrid3 extends Activity {
 		GridView gridview = (GridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new ImageAdapter(this)); 
 		
+		final ScaleAnimation scaleAnimation = new ScaleAnimation(1,2,1,2);
+		scaleAnimation.setDuration(3000);
+		
 		gridview.setOnItemClickListener(new OnItemClickListener() {
 	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	        	v.startAnimation(scaleAnimation);
 	        	Toast.makeText(MainActivityGrid3.this, "" + names[position], Toast.LENGTH_SHORT).show();
 	        }
 	    });
